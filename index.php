3b1436edef755413fb313deafd1c159b7b82efd7
notbadran <daffabadrann@gmail.com>
2024-09-01 19:58:05 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptas reprehenderit dolor nemo ipsum, dolore delectus veniam minus commodi minima quam molestiae assumenda voluptatem dolores sapiente, culpa, temporibus voluptatum iure.</p>
+    <ul>
+        <li>a</li>
+        <li>b</li>
+    </ul>
 </body>
 </html>
